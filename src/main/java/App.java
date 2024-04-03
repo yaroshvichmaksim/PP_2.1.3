87701f9fd898a4e756c8a016d027f359040d71a6
Maksim <yaroshevichmaks@gmail.com>
2024-04-03 21:42:43 +0300

    add project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat secondCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == secondBean);
+        System.out.println(firstCat == secondCat);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,14 +1,22 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
     }
 }
